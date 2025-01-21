30b5ef92a45b4db428094b5803c0db5fe3b61dc9
egapay <etg5588@rit.edu>
2025-01-20 22:55:10 -0500

    test scraping

---
+++ scrape.py
@@ -0,0 +1,20 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+PATH = "C:\\Program Files (x86)\\Google\\ChromeDrivers\\chromedriver.exe"
+service = Service(PATH)
+
+driver = webdriver.Chrome(service=service)
+driver.get("https://egapay.github.io/")
+
+search = driver.find_element(By.XPATH, "//div[@id='buttons']//button[text()='Skills']")
+print(search.text)
+time.sleep(2)
+search.send_keys(Keys.RETURN)
+time.sleep(5)
+
+driver.quit()
